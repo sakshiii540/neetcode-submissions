2fd24f79c047e48c2507d5476cc5610bd61ec9ae
sakshi <sakshi1505.becse24@chitkara.edu.in>
2026-07-06 23:55:26 +0530

    Bulk sync: 5 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length()!=t.length())return false;
+        int[] arr1 = new int[s.length()];
+        int[] arr2 = new int[t.length()];
+        for (int i=0;i<s.length();i++){
+            arr1[i]= s.charAt(i);
+        }
+         for (int i=0;i<t.length();i++){
+            arr2[i]= t.charAt(i);
+        }
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        for (int i=0;i<s.length();i++){
+            if (arr1[i]!=arr2[i])return false;
+        }
+        return true;
+    }
+}
